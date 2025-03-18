0dd80e5ff10ea5f8ca7d563c4eb58b7b772c5f29
RifqiAnshariR <ansharirifqi2@gmail.com>
2025-03-18 22:44:13 +0700

    Initial commit

--- algorithm/bfs_dsa.py
+++ algorithm/bfs_dsa.py
@@ -1,25 +1,59 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import numpy as np
+from algorithm.convert_coordinate import cartesianToIndex, indexToCartesian
 from collections import deque
 
 class GraphBFS:
     def __init__(self):
-        self.graph = {}
+        self.came_from = {}
 
-    def addEdge(self, node, neighbor):
-        if node not in self.graph:
-            self.graph[node] = []
-        self.graph[node].append(neighbor)
+    def isValid(self, height, width, neighbor, grid, visited):
+        return (0 <= neighbor[0] < height) and (0 <= neighbor[1] < width) and (grid[neighbor] == 0) and (neighbor not in visited)
 
-    def bfs(self, start):
+    def reconstructPath(self, height, start, goal):
+        path = []
+        current_idx = goal
+        while current_idx in self.came_from:
+            path.append(indexToCartesian(*current_idx, height))
+            current_idx = self.came_from[current_idx]
+        path.append(start)
+        return path[::-1]
+
+    def bfs(self, grid, start, goal):
+        height, width = grid.shape
+        start_idx = cartesianToIndex(*start, height)
+        goal_idx = cartesianToIndex(*goal, height)
+
         visited = set()
-        queue = deque([start])
-        visited.add(start)
+        queue = deque([start_idx])
+        visited.add(start_idx)
 
         while queue:
             node = queue.popleft()
-            print(node, end=" -> ")
-            for neighbor in self.graph.get(node, []):
-                if neighbor not in visited:
+
+            if node == goal_idx:
+                return self.reconstructPath(height, start, goal_idx)
+
+            row, col = node
+            for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:  # Atas, Bawah, Kiri, Kanan
+                neighbor = (row + dr, col + dc)
+
+                if self.isValid(height, width, neighbor, grid, visited):
                     visited.add(neighbor)
                     queue.append(neighbor)
+                    self.came_from[neighbor] = node
 
-graph = GraphBFS()
+        return None  # Jika tidak ada jalur yang ditemukan
+
+# **Eksekusi BFS dengan grid 5x5**
+grid = np.zeros((5, 5), dtype=int)
+bfs = GraphBFS()
+start = (3, 1)
+goal = (4, 4)
+
+path = bfs.bfs(grid, start, goal)
+print("Jalur:", path)
